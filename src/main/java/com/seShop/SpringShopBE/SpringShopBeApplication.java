3cefd775d0a6f530ec44b343ef32f99a3ca26380
788465h10 <dolongnhat0301@gmail.com>
2024-03-14 14:50:47 +0700

    initialirz spingboot

---
+++ src/main/java/com/seShop/SpringShopBE/SpringShopBeApplication.java
@@ -0,0 +1,13 @@
+package com.seShop.SpringShopBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShopBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seShop/SpringShopBE/SpringShopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seShop.SpringShopBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
